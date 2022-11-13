065ef6f38023cef9d8572420f2a62ed36875b7d3
vladgnedko62 <73539190+vladgnedko62@users.noreply.github.com>
2022-11-13 18:55:00 +0100

    Created animations.css
    Create animation on home page
    Create animate.css

--- resources/views/ClientPages/HomePage.blade.php
+++ resources/views/ClientPages/HomePage.blade.php
@@ -1,26 +1,38 @@
-
 @extends('layout/layout')
+@section('head')
+
+<title>Home</title>
+@endSection
 @section('content')
 
-    <div class="banner">
-        <div class="navbar">
-            <img src="../Logo2White.png" class="logo">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Notifications</a></li>
-                <li><a href="#">Some text</a></li>
-                <li><a href="#">Your cabinet</a></li>
-            </ul>
-        </div>
-        <div class="content">
-            <h1>Manage your notifications</h1>
-            <p>Manage your notifications with our help</p>
-            <div>
-                <button type="button"><span></span>HOW DOES IT WORK</button>
-                <button type="button"><span></span>START</button>
+<div class="banner">
+    <div class="navbar animated fadeInLeft">
+        <img src="../Logo2White.png" class="logo">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Notifications</a></li>
+            <li><a href="#">Some text</a></li>
+            <li><a href="#">Your cabinet</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h1>Manage your notifications</h1>
+        <p>Manage your notifications with our help</p>
+        <div>
+            <div class="homeButtons">
+                <div class="homeButton  animated  rotateInDownLeft">
+                    <button type="button"><span></span>HOW DOES IT WORK</button>
+                </div>
+                <div class="homeButton animated  rotateInDownRight">
+                    <button type="button"><span></span>START</button>
+                </div>
             </div>
+
+
+
         </div>
     </div>
-    <div class="part2"></div>
+</div>
+<div class="part2"></div>
 
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
         <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 
     <link rel="stylesheet" href="./css/home.css" type="text/css">
-    
+    <link rel="stylesheet" href="./css/animate.css" type="text/css">
+    @yield("head")
 </head>
 <body>
 

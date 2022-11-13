186f9fe0f7b42c19e66b078d0ecd0cc17768a159
Phanttt <117937309+Phanttt@users.noreply.github.com>
2022-11-13 21:36:16 +0100

    HomeStartVersion

--- resources/views/ClientPages/HomePage.blade.php
+++ resources/views/ClientPages/HomePage.blade.php
@@ -21,12 +21,18 @@
             <p>Manage your notifications.</p>
             <div>
                 <button class="animated zoomIn" type="button"><span></span>HOW DOES IT WORK</button>
-                <button class="animated zoomIn" type="button"><span></span>START</button>
+                <button class="animated zoomIn" type="button"><span></span>START</button> 
+                <!-- infinite pulse -->
             </div>
         </div>
     </div>
     <div class="part2">
-        <h1>How does it work</h1>
+    <h1>How does it work</h1>
+        <img src="../clocks.png" class="clock">
+        
+        <div class="aboutUs">
+            The most important thing in our life is time. We provide an online task manager. With it, you can regulate your plans, sort them, see future and completed ones.
+        </div>
     </div>
 
 @endsection

ab2eb47c8ca932be41ed6aed87abfe6b400c1982
Hovis22 <71640623+Hovis22@users.noreply.github.com>
2022-11-13 22:19:07 +0200

    Database Create
    Users Table;
    Tags Table;
    Alerts Table;
    Images Table;

---
+++ database/migrations/2022_11_13_195030_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->unsignedBigInteger('alert_id')->index();
+            $table->timestamps();
+
+
+            $table->foreign('alert_id')->references('id')->on('alerts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};

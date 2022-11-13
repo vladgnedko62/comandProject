b2c1621c801229a715fb0575c86f223c1c85e186
Phanttt <117937309+Phanttt@users.noreply.github.com>
2022-11-13 18:15:47 +0100

    Добавил Home, latiut, css

---
+++ resources/views/ClientPages/HomePage.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layouts/layoutUser')
+@section('content')
+
+    <div class="banner">
+        <div class="navbar">
+            <img src="./images/Logo2White.png" class="logo">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Notifications</a></li>
+                <li><a href="#">Some text</a></li>
+                <li><a href="#">Your cabinet</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Manage your notifications</h1>
+            <p>Manage your notifications with our help</p>
+            <div>
+                <button type="button"><span></span>HOW DOES IT WORK</button>
+                <button type="button"><span></span>START</button>
+            </div>
+        </div>
+    </div>
+    <div class="part2"></div>
+
+@endsection

c11248ee2fc01b88cf2d0b6c4b6118860189d699
vladgnedko62 <73539190+vladgnedko62@users.noreply.github.com>
2022-11-14 17:36:22 +0100

    Created beta pay system
    -New table payments
    -PaymentController
    -Updated home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\PayPalController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,"home"]);
+
+Route::get('pay', [PaymentController::class, 'pay'])->name('payment');
+Route::get('success', [PaymentController::class, 'success']);
+Route::get('error', [PaymentController::class, 'error']);
+
+

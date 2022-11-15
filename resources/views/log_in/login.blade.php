c7c9b2f915cb3ae10c69b7531e47cd0f723f2913
Phanttt <117937309+Phanttt@users.noreply.github.com>
2022-11-15 21:52:26 +0100

    qwe

--- resources/views/log_in/login.blade.php
+++ resources/views/log_in/login.blade.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endSection
 @section('content')
+<div class="mainBanner">
 <div class="banner">
         <div class="navbar animated">
             <img src="images/logoMain.png" class="logo" onclick="javascript:location.href='/'">
@@ -48,6 +49,25 @@
           
           <p class="userN">New user? <a id="reg" href="/register">Register</a></p>
         </div>
+    </div>
+    <section class="footer">
+        <div class="social">
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-snapchat"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p class="copyright">
+            Future Coders @ 2021
+        </p>
+    </section>
     </div>
     <script src="jsFiles/loginPageAnim.js"></script>
 @endsection

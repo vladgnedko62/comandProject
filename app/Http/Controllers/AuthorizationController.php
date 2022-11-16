ad852b5cac5010ac7c8fd44aba210a91a6fd9d9f
Hovis22 <71640623+Hovis22@users.noreply.github.com>
2022-11-16 06:51:35 +0200

    Login/Register Update
    Google
    GitHub

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -2,38 +2,72 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+
+use App\Models\User;
+
 class AuthorizationController extends Controller
 {
-    public function continueWithGoogle()
-    {
-        return Socialite::driver("google")->stateless()->redirect();
-    }
-    public function registartionOrLoginWithGoogle()
-    {
-        $user = Socialite::driver('google')->stateless()->user();
 
+    public function registartionOrLoginWith()
+    {
+        $id_name = "";
+        $uri = Route::currentRouteName();
+        $user = Socialite::driver($uri)->stateless()->user();
+         
+      
+         
+       if($user->name==NULL){
+        return redirect()->to(route(name:'user.login'))->withErrors([
+            'name' => "Pick Name"
+        ]);
+       }
+
+
+        if($uri=='google')$id_name="google_id";
+        else if($uri=='github')$id_name="github_id";
+
+        $isUser = User::where($id_name,$user->id)->first();
+
+
+
+        if($isUser){
+            Auth::login($isUser);
+
+            return redirect('/');
+        }
+        else{
+            $createUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                $id_name => $user->id,
+                'password' => encrypt('user')
+            ]);
+
+
+            Auth::login($createUser);
+
+                 return redirect('/');
+        }
+        return redirect('/');
+       
         
     }
-    public function continueWithGitHub()
-    {
-        return Socialite::driver("github")->stateless()->redirect();
-    }
-    public function registartionOrLoginWithGitHub()
-    {
-        $user = Socialite::driver('github')->stateless()->user();
-        
-        dd($user);
-    }
-    public function continueWithLinkedIn()
-    {
-        return Socialite::driver("linkedin")->stateless()->redirect();
-    }
-    public function registartionOrLoginWithLinkedIn()
-    {
-        $user = Socialite::driver('linkedin')->stateless()->user();
-        
-        dd($user);
-    }
+
+
+
+     public function continueWith(){
+        $uri = Route::currentRouteName();
+        return Socialite::driver($uri)->stateless()->redirect();
+     }
+ 
+    
+
+
+
+
 }

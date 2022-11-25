eb78e2a76f43ebf426c318aa82eb4b23b4e4405c
Hovis22 <71640623+Hovis22@users.noreply.github.com>
2022-11-25 21:52:53 +0200

    AlertController Create
    index
    create
    store

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Alerts;
+use App\Models\Images;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use DateTime;
+
 class AlertController extends Controller
 {
     public function index(){
@@ -15,7 +19,60 @@ class AlertController extends Controller
     }
 
     public function create(){
-        return view('alerts.create');
+        $tags = Tag::all();
+
+        return view('alerts.create',compact('tags'));
+   }
+
+   public function store(Request $req){
+    
+     $alert = new Alerts();
+
+     $alert->alert = $req->get("alertName");
+     $alert->tag_id = $req->get("tag");
+     $alert->user_id = Auth::user()->id;
+     $alert->start_date =  $req->get("startDate");
+     $alert->end_date =  $req->get("endDate");
+     $alert->complete = false;
+     $alert->created_at = new DateTime();
+
+     $alert->save();
+
+
+     for($i=1;$i<=3;$i++){
+        $image =  new Images();
+
+        if($req->file('image'. $i) !=null){
+        $name = $req->file('image'. $i )->getClientOriginalName();
+     
+      $req->file('image' . $i)->storeAs('public/images/',$name);
+
+      $image->image = $name;
+      $image->alert_id = $alert->id; 
+      $image->created_at = new DateTime();
+    
+     
+      $image->save();
+    }
+     }
+
+    //     $image =  new Images();
+
+    //     $name = $req->file('image1')->getClientOriginalName();
+     
+    //   $req->file('image1')->storeAs('public/images/',$name);
+
+    //   $image->image = $name;
+    //   $image->alert_id = $alert->id; 
+    //   $image->created_at = new DateTime();
+    
+    
+    //     
+    
+
+ 
+        
+    return redirect("/private");
    }
 
 
